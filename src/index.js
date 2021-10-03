6302dd274c64ab4b8f64816d919b4b5bcf745a95
Coby <coby@lakefox.net>
2021-10-03 18:36:59 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const server = require('./config/server')
-const { TCPClient, Packet } = require('dns2')
+const { Packet } = require('dns2')
+
+server.on('request', (req, send, res) => {
+    console.log(req.header.id, req.questions[0])
+    const response = Packet.createResponseFromRequest(req)
+    const [ question ] = req.questions
+    const { name } = question
+    response.answers.push({
+        name,
+        type: Packet.TYPE.A,
+        class: Packet.CLASS.IN,
+        ttl: 300,
+        address: result.answers.address
+    })
+    send(response)
+})
 
 (async () => {
     const closed = new Promise(resolve => process.on("SIGINT", resolve))
@@ -9,25 +24,6 @@ const { TCPClient, Packet } = require('dns2')
     })
     console.log("Listening for requests")
     console.log(server.addresses())
-
-    let resolve = TCPClient({ dns: '1.1.1.1' })
-    let result = await resolve(res.questions[0].name)
-
-    server.on('request', (req, send, res) => {
-        console.log(req.header.id, req.questions[0])
-        const response = Packet.createResponseFromRequest(req)
-        const [ question ] = req.questions
-        const { name } = question
-        response.answers.push({
-            name,
-            type: Packet.TYPE.A,
-            class: Packet.CLASS.IN,
-            ttl: 300,
-            address: result.answers.address
-        })
-        send(response)
-    })
-
     await closed
     process.stdout.write('\n')
     await server.close()
