8ce03dbe2181e45300c0e24359f8fbb96a9203cb
Coby <coby@lakefox.net>
2021-10-04 01:07:04 -0500

    Bug Fixes

--- src/config/server.js
+++ src/config/server.js
@@ -5,47 +5,49 @@ const { port } = require('./vars')
 const { TCPClient, Packet } = require('dns2')
 const { spawn } = require('child_process')
 
-const server = dns2.createServer({
-    udp: true,
-    tcp: true,
-    doh: {
-        ssl: true,
-        cert: fs.readFileSync(path.join(__dirname, '../../server-cert.pem')),
-        key: fs.readFileSync(path.join(__dirname, '../../server-key.pem'))
-    },
-    handle: async (req, send, client) => {
-        console.log(client)
-        const response = Packet.createResponseFromRequest(req)
-        const [ question ] = req.questions
-        const { name } = question
-
-        let tlds = fs.readFileSync(path.join(__dirname, "../../icann_domains.txt")).toString()
-        tlds = tlds.split("\n").slice(1)
+(async () => {
+    const server = dns2.createServer({
+        udp: true,
+        tcp: true,
+        doh: {
+            ssl: true,
+            cert: fs.readFileSync(path.join(__dirname, '../../server-cert.pem')),
+            key: fs.readFileSync(path.join(__dirname, '../../server-key.pem'))
+        },
+        handle: async (req, send, client) => {
+            console.log(client)
+            const response = Packet.createResponseFromRequest(req)
+            const [ question ] = req.questions
+            const { name } = question
     
-        if (tlds.indexOf(name.split('.').pop().split('/')[0].toUpperCase()) >= 0) {
-            const resolve = TCPClient({ dns: '1.1.1.1' })
-            const result = await resolve(response.questions[0].name)
-            response.answers = result.answers;
-            send(response)
-        } else {
-            const child = spawn('/root/hsd/bin/hsd-cli', ['rpc', 'getnameresource', name.split('.')[1], '--api-key=menace'])
-            child.stdout.on('data', async (data) => {
-                if (data.toString()[0] != 'I') {
-                    let json = JSON.parse(data.toString())
-                    if (typeof json == 'object' && json != null) {
-                        if (typeof json.records == 'object') {
-                            let resolveHS = TCPClient({
-                                dns: json.records[0].address
-                            })
-                            let result = await resolveHS(response.questions[0].name)
-                            response.answers = result.answers
-                            send(response)
+            let tlds = fs.readFileSync(path.join(__dirname, "../../icann_domains.txt")).toString()
+            tlds = tlds.split("\n").slice(1)
+        
+            if (tlds.indexOf(name.split('.').pop().split('/')[0].toUpperCase()) >= 0) {
+                const resolve = TCPClient({ dns: '1.1.1.1' })
+                const result = await resolve(response.questions[0].name)
+                response.answers = result.answers;
+                send(response)
+            } else {
+                const child = spawn('/root/hsd/bin/hsd-cli', ['rpc', 'getnameresource', name.split('.')[1], '--api-key=menace'])
+                child.stdout.on('data', async (data) => {
+                    if (data.toString()[0] != 'I') {
+                        let json = JSON.parse(data.toString())
+                        if (typeof json == 'object' && json != null) {
+                            if (typeof json.records == 'object') {
+                                let resolveHS = TCPClient({
+                                    dns: json.records[0].address
+                                })
+                                let result = await resolveHS(response.questions[0].name)
+                                response.answers = result.answers
+                                send(response)
+                            }
                         }
                     }
-                }
-            })
+                })
+            }
         }
-    }
+    })
 })
 
 module.exports = server
