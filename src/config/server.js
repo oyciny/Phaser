9ac6887f8ebd72f0c38bdfaa651d79fdb777d260
Coby <coby@lakefox.net>
2021-10-04 00:51:41 -0500

    Bug Fixes

--- src/config/server.js
+++ src/config/server.js
@@ -39,7 +39,7 @@ const server = dns2.createServer({
                             let result = await resolveHS(response.questions[0].name)
                             console.log(result.answers)
                             console.log(response.answers)
-                            response.answers = response.answers.concat(result.answers)
+                            response.answers = result.answers[0]
                             console.log(response.answers)
                         }
                     }
