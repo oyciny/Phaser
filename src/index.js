6bc1eafe45620afdcd2154c72f4dec449849b6b1
Coby <coby@lakefox.net>
2021-10-04 01:14:12 -0500

    Bug Fixes

--- src/index.js
+++ src/index.js
@@ -14,6 +14,38 @@ server.on("listening", () => {
 
 server.on('request', async (req, send, client) => {
     console.log(`ID: ${req.header.id} | Name: ${req.questions[0].name}`)
+
+    console.log(client, " : ", req.questions[0])
+    const response = Packet.createResponseFromRequest(req)
+    const [ question ] = req.questions
+    const { name } = question
+
+    let tlds = fs.readFileSync(path.join(__dirname, "../../icann_domains.txt")).toString()
+    tlds = tlds.split("\n").slice(1)
+    
+    if (tlds.indexOf(name.split('.').pop().split('/')[0].toUpperCase()) >= 0) {
+        const resolve = TCPClient({ dns: '1.1.1.1' })
+        const result = await resolve(response.questions[0].name)
+        response.answers = result.answers;
+        send(response)
+    } else {
+        const child = spawn('/root/hsd/bin/hsd-cli', ['rpc', 'getnameresource', name.split('.')[1], '--api-key=menace'])
+        child.stdout.on('data', async (data) => {
+            if (data.toString()[0] != 'I') {
+                let json = JSON.parse(data.toString())
+                if (typeof json == 'object' && json != null) {
+                    if (typeof json.records == 'object') {
+                        let resolveHS = TCPClient({
+                            dns: json.records[0].address
+                        })
+                        let result = await resolveHS(response.questions[0].name)
+                        response.answers = result.answers
+                        send(response)
+                    }
+                }
+            }
+        })
+    }
 })
 
 server.on('close', () => {
