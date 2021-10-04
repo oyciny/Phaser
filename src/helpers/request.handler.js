94ccaa6ccb6a3ae07ad762d1b607d75fa015bd23
Coby <coby@lakefox.net>
2021-10-03 21:55:50 -0500

    Update

--- src/helpers/request.handler.js
+++ src/helpers/request.handler.js
@@ -1,8 +1,8 @@
 const { TCPClient, Packet } = require('dns2')
 
-let request = async (request) => {
-    const response = Packet.createResponseFromRequest(request)
-    const [ question ] = request.question
+let request = async (req) => {
+    const response = Packet.createResponseFromRequest(req)
+    const [ question ] = req.question
     const { name } = question
     const resolve = TCPClient({ dns: '1.1.1.1' })
     const result = await resolve(name)
